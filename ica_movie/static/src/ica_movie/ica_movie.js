faafcac6754d0a3b395266aca47ae406141bcf72
Agga <agga@Aggas-MacBook-Air.local>
2024-08-07 22:09:31 +0700

    search,update,delete

--- ica_movie/static/src/ica_movie/ica_movie.js
+++ ica_movie/static/src/ica_movie/ica_movie.js
@@ -1,27 +1,44 @@
 /** @odoo-module **/
 
 import {registry} from "@web/core/registry";
-import {useService} from "@web/core/utils/hooks";
-import {Component, useState,onWillStart} from "@odoo/owl";
+import {Component, useState, onWillStart, useRef} from "@odoo/owl";
 
 class IcaMovieAction extends Component {
+
     setup() {
+        this.nameRef = useRef('name');
         this.state = useState({
             partners: [],
         })
-        this.orm = useService('orm');
+        this.resModel = 'res.partner';
+        this.orm = this.env.services.orm;
         onWillStart(async () => {
             await this.getAllPartners();
         })
     }
 
-    async getAllPartners() {
-        // console.log("search partner");
-        this.state.partners = await this.orm.searchRead('res.partner', [], ['id', 'name']);
-        // console.log(partners);
-        //  = partners;
+    async searchPartners(e) {
+        if (e.type === 'click' || e.keyCode === 13) {
+            let name = this.nameRef.el.value;
+            await this.getAllPartners(name);
+            this.nameRef.el.value = null;
+        }
     }
 
+    async getAllPartners(name) {
+        const domain = [['name', 'ilike', name]];
+        const fields = ['id', 'name', 'email', 'phone'];
+        this.state.partners = await this.orm.searchRead(this.resModel, domain, fields);
+    }
+
+    async deletePartner(newPartner) {
+        await this.orm.write(this.resModel, [newPartner.id], {active: false});
+        this.state.partners = this.state.partners.filter(partner => partner.id !== newPartner.id);
+    }
+
+    async updatePartner(newPartner) {
+        console.log(newPartner);
+    }
 
 }
 
