c146bd357a60bc5e78a71ceba6bc4c345a35f87f
Agga <agga@Aggas-MacBook-Air.local>
2024-08-27 20:20:11 +0700

    add

--- ica_movie/controllers/main.py
+++ ica_movie/controllers/main.py
@@ -1,5 +1,6 @@
 from odoo import http
 from odoo.http import request
+import odoo
 
 
 class MainController(http.Controller):
@@ -11,18 +12,17 @@ class MainController(http.Controller):
 
     @http.route('/ica/send-bus', type='json', auth='user')
     def send_bus(self, **kw):
-        print(kw)
         request.env['bus.bus']._sendone(
             'ica-movie-channel',
             'ica-movie-channel/sending-message', kw)
         return True
 
     @http.route("/ica-movie/standalone_app", auth="public")
-    def standalone_app(self):
+    def standalone_app(self, **kw):
+        # get_frontend_session_info: dict = request.env['ir.http'].get_frontend_session_info()
+        data = {
+            'session_info': {'user_context': {'lang': 'my_MM', }},
+        }
         return request.render(
-            'ica_movie.standalone_app',
-            {
-                'session_info': request.env['ir.http'].get_frontend_session_info(),
-                # 'session_info': request.env['ir.http'].get_session_info(),
-            }
+            'ica_movie.standalone_app', data
         )
--- ica_movie/static/src/standalone_app/components/sale_orders/sale_orders.js
+++ ica_movie/static/src/standalone_app/components/sale_orders/sale_orders.js
@@ -1,10 +1,21 @@
 /** @odoo-module */
 import {Component, onWillStart, useState} from "@odoo/owl";
-import {Customers} from "../customers/customers";
-import { registry } from "@web/core/registry";
+import {registry} from "@web/core/registry";
+import {_t} from "@web/core/l10n/translation";
 
 export class SaleOrders extends Component {
     static template = "ica_movie.SaleOrders";
     static props = {};
+
+    setup() {
+        this.state = useState({
+            translateText: _t("ICA"),
+        })
+    }
+
+    getClick(){
+        console.log("heloo");
+    }
 }
-registry.category('ica.movie').add('saleOrderScreen',SaleOrders)
+
+registry.category('ica.movie').add('saleOrderScreen', SaleOrders)
